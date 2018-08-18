fee9531d60449ccfe15c422931e24fade3e5ea86
zori0 <776021342@qq.com>
2018-08-18 22:28:55 +0800

    优化

---
+++ pages/broadcast/work-news.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <div class="data-box">
+      <div class="left-box">
+          <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+          <div class="title-box">
+            测试发布测试发布测试发布测试发布测试发布
+          </div>
+          <div class="date-box">
+            <label >2019-01-01</label>
+            <label>1012<span>阅</span></label>
+          </div>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <div class="left-box">
+        <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+        <div class="title-box">
+          测试发布测试发布测试发布测试发布测试发布
+        </div>
+        <div class="date-box">
+          <label >2019-01-01</label>
+          <label>1012<span>阅</span></label>
+        </div>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <div class="left-box">
+        <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+        <div class="title-box">
+          测试发布测试发布测试发布测试发布测试发布
+        </div>
+        <div class="date-box">
+          <label >2019-01-01</label>
+          <label>1012<span>阅</span></label>
+        </div>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <div class="left-box">
+        <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+        <div class="title-box">
+          测试发布测试发布测试发布测试发布测试发布
+        </div>
+        <div class="date-box">
+          <label >2019-01-01</label>
+          <label>1012<span>阅</span></label>
+        </div>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <div class="left-box">
+        <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+        <div class="title-box">
+          测试发布测试发布测试发布测试发布测试发布
+        </div>
+        <div class="date-box">
+          <label >2019-01-01</label>
+          <label>1012<span>阅</span></label>
+        </div>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <div class="left-box">
+        <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+        <div class="title-box">
+          测试发布测试发布测试发布测试发布测试发布
+        </div>
+        <div class="date-box">
+          <label >2019-01-01</label>
+          <label>1012<span>阅</span></label>
+        </div>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <div class="left-box">
+        <img src="../../assets/images/jd.png"/>
+      </div>
+      <div class="right-box">
+        <div class="title-box">
+          测试发布测试发布测试发布测试发布测试发布
+        </div>
+        <div class="date-box">
+          <label >2019-01-01</label>
+          <label>1012<span>阅</span></label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+
+  .left-box{
+    display: inline-block;
+    width:33%;
+    float: left;
+    margin-right: 10px;
+  }
+
+  .left-box img {
+    width: 232px;
+    height: 144px;
+    border-radius: 6px;
+
+  }
+
+  .right-box{
+    display: inline-block;
+    width:63%;
+    height: 144px;
+
+  }
+
+  .title-box{
+    font-size: 14px;
+    min-height: 108px;
+  }
+
+  .data-box{
+    padding:30px 14px;
+    border-bottom:1px solid #979797;
+    height: 160px;
+  }
+
+  .date-box label{
+    font-size: 8px;
+    margin-right: 20px;
+    color: #9e9e9e;
+  }
+</style>
