ed449270f92152fd2c4b836ec2c57ed7200a7324
zori0 <776021342@qq.com>
2018-08-26 07:54:52 +0800

    优化

---
+++ components/select-place-box.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div class="select-box">
+      <input type="radio" name="place" class="select-type" v-bind:value="item.value" />
+      <i class="select-type-i" :class="item.icon+'-i'">{{item.name}}</i>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name: "select-place-box",
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+</script>
+<style>
+  .select-box {
+
+    font-style: normal;
+    margin-top: 66px;
+    width: 666px;
+    height: 160px;
+    border-radius: 10px;
+    border:6px solid #C9BABA;
+    text-align: center;
+    position: relative;
+  }
+
+  .select-type{
+    opacity: 0;
+    border: 2px solid black;
+    width: 666px;
+    height: 160px;
+    position: relative;
+    top: 0;
+    left: 0;
+    cursor: pointer;
+    z-index: 9;
+  }
+
+
+  .select-type:checked+i {
+    /*background-color: #FFFFFF;*/
+    /*border: 2px solid #EF5104;*/
+    /*border-radius: 4px;*/
+
+
+  }
+  .select-type:checked+i:after{
+    content: "";
+    width: 200px;
+    height: 200px;
+    background: url("~/assets/images/selected.png") no-repeat;
+    background-size: 210px 164px;
+    display: block;
+    position: absolute;
+    right: 18px;
+    top: 0;
+  }
+
+  .select-type-i{
+    width: 666px;
+    height:160px;
+    font-size:48px;
+    font-family:PingFangSC-Light;
+    font-weight:300;
+    color:rgba(86,83,83,1);
+    line-height:160px;
+    font-style: normal;
+    position: absolute;
+    top: 0;
+    left: 20px;
+  }
+
+  .school-i {
+    background-size: 108px 108px;
+    background: url("~/assets/images/school.png") no-repeat 40px 20px;
+  }
+
+  .public-i{
+    background-size: 108px 108px;
+    background: url("~/assets/images/public.png") no-repeat 40px 40px;
+  }
+
+  .medical-i{
+    background-size: 108px 108px;
+    background: url("~/assets/images/medical.png") no-repeat 40px 20px;
+  }
+
+  .water-i{
+    background-size: 108px 108px;
+    background: url("~/assets/images/water.png") no-repeat 40px 20px;
+  }
+</style>
--- pages/honesty/honesty.vue
+++ pages/honesty/honesty.vue
@@ -1,9 +1,57 @@
 <template>
-  <div></div>
+  <div class="box">
+    <div class="selected-title">您是:</div>
+    <select-place-box v-for="item in items" :key="item.value" :item="item"></select-place-box>
+  </div>
 </template>
 <script>
-
+ import SelectPlaceBox from '~/components/select-place-box.vue'
+ export default{
+     name:"honesty",
+     components:{
+       SelectPlaceBox
+     },
+     data(){
+         return {
+             items:[
+               {
+                  name:"学校卫生",
+                  value:"1",
+                  icon:"school"
+               },
+               {
+                 name:"公共场所",
+                 value:"2",
+                 icon:"public"
+               },
+               {
+                 name:"医疗机构",
+                 value:"3",
+                 icon:"medical"
+               },
+               {
+                 name:"供水单位",
+                 value:"4",
+                 icon:"water"
+               }
+             ]
+         }
+     }
+ }
 </script>
 <style>
 
+  .box{
+    padding: 0 42px;
+  }
+  .selected-title{
+    padding-top: 46px;
+    padding-left: 3px;
+    height:66px;
+    font-size:48px;
+    font-family:PingFangSC-Light;
+    font-weight:300;
+    color:rgba(86,83,83,1);
+    line-height:66px;
+  }
 </style>
